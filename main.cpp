f413e787861c8cd471435deb2e76dfac1769b636
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 15:07:25 -0500

    Create the main.cpp as the core of the project to get I/O from one thread and have a cnf-sat in another thread.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
+#include <sstream>
 #include "helpers.h"
 #include "MyGraph.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+bool g_cancel;
+pthread_mutex_t g_lock;
+pthread_cond_t g_cond;
+
+typedef struct MyGraph threadGraph;
+
+void *cnf_sat(void *data)
+{
+    //threadGraph *new_graph = static_cast<threadGraph *>(data);
+    MyGraph *new_graph = (MyGraph *)data;
+    while (!g_cancel)
+    {
+        pthread_mutex_lock(&g_lock);
+        if (new_graph->edges.size() > 0)
+        {
+            new_graph->getVertexCover();
+            new_graph->printVertexCover(true);
+            new_graph->resetEverything();
+            new_graph->setSize(0);
+            
+        }
+        //std::cout << "I am here" << std::endl << std::flush;
+        pthread_cond_wait(&g_cond, &g_lock);
+        pthread_mutex_unlock(&g_lock);
+        //usleep(1000);
+    }
+    return nullptr;
+}
 
 int main(int argc, char **argv) 
 {
+    pthread_mutex_init(&g_lock, nullptr);
+    pthread_cond_init(&g_cond, nullptr);
+
+    g_cancel = false;
+    int ret = 0;
+    pthread_t thread1;
+
     MyGraph graph;
+    bool first = true;
     while (!std::cin.eof())
     {
         std::string line;
@@ -12,12 +54,35 @@ int main(int argc, char **argv)
         std::string command = line_parser::get_command(line, graph);
         line_parser::analyze_command(command, line, graph);
         if (graph.edges.size() > 0)
+        {
+            if (first)
             {
-                graph.getVertexCover();
-                graph.printVertexCover();
-                graph.resetEverything();
+                ret = pthread_create(&thread1, nullptr, cnf_sat, (void *)&graph);
+                if (ret != 0)
+                {
+                    std::cout << "Error: pthread_create() failed\n";
+                    exit(EXIT_FAILURE);
+                }
+                first = false;
             }
+            else
+            {
+                pthread_mutex_lock(&g_lock);
+                pthread_cond_signal(&g_cond);
+                pthread_mutex_unlock(&g_lock);
+            }
+        }
         
+    }
+    g_cancel = true;
+    pthread_mutex_lock(&g_lock);
+    pthread_cond_signal(&g_cond);
+    pthread_mutex_unlock(&g_lock);
+    ret = pthread_join(thread1, nullptr);
+    if (ret != 0)
+    {
+        std::cout << "Error: pthread_join() failed\n";
+        exit(EXIT_FAILURE);
     }
     
  return 0;
